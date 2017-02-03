5d5de2450c4262d8d253de9810f4b95ff51d44ab
David Sherline <david@hypsol.com>
2017-02-03 15:00:53 -0600

    No need to create a variable

--- app/jobs/earthquake_import_job.rb
+++ app/jobs/earthquake_import_job.rb
@@ -3,8 +3,7 @@ class EarthquakeImportJob < ApplicationJob
 
   def perform(csv_chunk)
     csv_chunk.each do |row|
-      earthquake = EarthquakeTranslator.new(row).to_earthquake
-      earthquake.save
+      EarthquakeTranslator.new(row).to_earthquake.save
     end
   end
 end
