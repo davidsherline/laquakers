1bbab423011a89e943d48eb4e1f23f5fcf2068fd
David Sherline <david@hypsol.com>
2017-02-03 14:32:09 -0600

    Refactor tests to not check return from method

--- spec/jobs/earthquake_import_job_spec.rb
+++ spec/jobs/earthquake_import_job_spec.rb
@@ -2,34 +2,40 @@ require 'rails_helper'
 
 RSpec.describe EarthquakeImportJob, type: :job do
   describe '.perform' do
-    let(:valid_csv_row) { build(:csv_row) }
-    let(:invalid_csv_row) { build(:csv_row, :invalid) }
-
     context 'with a new record' do
+      before(:each) do
+        EarthquakeImportJob.perform_now([csv_row])
+      end
+
       context 'with valid data' do
+        let(:csv_row) { build(:csv_row) }
+
         it 'should create an Earthquake' do
-          expect(EarthquakeImportJob.perform_now([valid_csv_row])).to be true
           expect(Earthquake.count).to eq(1)
         end
       end
 
       context 'with invalid data' do
+        let(:csv_row) { build(:csv_row, :invalid) }
+
         it 'should not create an Earthquake' do
-          expect(EarthquakeImportJob.perform_now([invalid_csv_row]))
-            .to be false
           expect(Earthquake.count).to eq(0)
         end
       end
     end
 
     context 'with an old record' do
+      let(:old_record) { build(:csv_row) }
+
       before(:each) do
-        EarthquakeImportJob.perform_now([valid_csv_row])
+        EarthquakeImportJob.perform_now([old_record])
+        EarthquakeImportJob.perform_now([new_record])
       end
 
       context 'with the same data' do
+        let(:new_record) { old_record }
+
         it 'should update the Earthquake' do
-          expect(EarthquakeImportJob.perform_now([valid_csv_row])).to be true
           expect(Earthquake.count).to eq(1)
         end
       end
@@ -37,23 +43,19 @@ RSpec.describe EarthquakeImportJob, type: :job do
       context 'with updated data' do
         context 'with valid data' do
           let(:now) { Time.now }
-          let(:updated_csv_row) { build(:csv_row, updated: now) }
+          let(:new_record) { build(:csv_row, updated: now) }
 
           it 'should update the Earthquake' do
-            expect(EarthquakeImportJob.perform_now([updated_csv_row]))
-              .to be true
             expect(Earthquake.count).to eq(1)
             expect(Earthquake.first.updated_at).to eq(now)
           end
         end
 
         context 'with invalid data' do
-          let(:valid_csv_row) { build(:csv_row, mag: 2) }
-          let(:invalid_csv_row) { build(:csv_row, :invalid, mag: 1) }
+          let(:old_record) { build(:csv_row, mag: 2) }
+          let(:new_record) { build(:csv_row, :invalid, mag: 1) }
 
           it 'should not update the Earthquake' do
-            expect(EarthquakeImportJob.perform_now([invalid_csv_row]))
-              .to be false
             expect(Earthquake.count).to eq(1)
             expect(Earthquake.first.magnitude).to eq(2)
           end
@@ -62,6 +64,9 @@ RSpec.describe EarthquakeImportJob, type: :job do
     end
 
     context 'with one valid and one invalid record' do
+      let(:valid_csv_row) { build(:csv_row) }
+      let(:invalid_csv_row) { build(:csv_row, :invalid) }
+
       before(:each) do
         EarthquakeImportJob.perform_now([valid_csv_row, invalid_csv_row])
       end
